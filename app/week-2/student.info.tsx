fb2902a816c3abcbc09b2a8ec1586ea90fce4a6d
fevenYtbarek <feven.ytbarek@edu.sait.ca>
2026-01-22 15:42:18 -0700

    cprg306 week 2 assignment

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,8 @@
+export default function studentInfo() {
+  return (
+    <div>
+      <h2>Name: Feven Ytbarek</h2>
+      <h3> <a href="https://github.com/fevenYtbarek/cprg306-assignments"> GitHub: fevenYtbarek/cprg306-assignments </a></h3>
+    </div>
+  );
+}
