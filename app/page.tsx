2926d0393879ca0ceda1db81d28de2fc20027329
fevenYtbarek <feven.ytbarek@edu.sait.ca>
2026-01-29 13:30:29 -0700

    made chnages

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-
+ 
 export default function Home() {
   return (
-    <main> 
-      <h1>CPRG 306: Web Development 2 - Assignments</h1> 
-      <Link href="/week-2"> Week 2 Page</Link>
-      <Link href="/week-3"> Week 3 Page</Link>
-    </main> 
-
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2">Go to Week 2 Page</Link>
+    </main>
   );
 }
+ 
--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
+import StudentInfo from "./student.info";
 
-export default function Home() {
-  return (
-    <main> 
-      <h1>CPRG 306: Web Development 2 - Assignments</h1> 
-      <Link href="/week-2"> Week 2 Page</Link>
-      <Link href="/week-3"> Week 3 Page</Link>
-    </main> 
-
-  );
+export default function page() {
+    return (
+        <main>
+            <h1>Shopping list</h1>
+            <StudentInfo />
+        </main>
+    );
 }
