2e1ddd2515e969e2d60a389b38dca143c24bfe59
fevenYtbarek <feven.ytbarek@edu.sait.ca>
2026-01-29 13:39:43 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
- 
+
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Go to Week 2 Page</Link>
+      <br />
       <Link href="/week-3">Go to Week 3 Page</Link>
     </main>
   );
 }
- 
