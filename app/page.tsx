2a36f9ddc195dad7a5149a5bf2d067ac7ee6f435
fevenYtbarek <feven.ytbarek@edu.sait.ca>
2026-04-01 16:32:47 -0600

    updated week-8

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,8 @@ export default function Home() {
       <Link href="/week-6">Go to Week 6 Page</Link>
       <br />
       <Link href="/week-7">Go to Week 7 Page</Link>
+      <br />
+      <Link href="/week-8">Go to Week 8 Page</Link>
     </main>
   );
 }
--- app/week-8/page.js
+++ app/week-8/page.js
@@ -24,7 +24,7 @@ export default function Page() {
 
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Shopping List App</h1>
+      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
 
       {!user ? (
         <div>
